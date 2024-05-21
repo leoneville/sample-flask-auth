9c2cda9913f2ae14973399289c480980a5b6514c
leoneville <leoneville_@hotmail.com>
2024-05-21 17:47:21 -0300

    Refactor: Modularize database setup and introduce User model

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+
+from database import db
+from models import User
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db'
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
 
 @app.route("/hello-world", methods=["GET"])
