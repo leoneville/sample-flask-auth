81bd31fe57f41d7799aad0335c50ace6c56f9a0d
leoneville <leoneville_@hotmail.com>
2024-05-21 18:04:22 -0300

    feat: Implement login endpoint

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_login import LoginManager
 
 from database import db
 from models import User
@@ -7,7 +8,27 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db'
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+
+# view login
+
+
+@app.route("/login", methods=['POST'])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            ...
+
+        return {"message": "Usuário autenticado com sucesso."}, 200
+
+    return jsonify({"message": "Credenciais inválidas"}), 400
 
 
 @app.route("/hello-world", methods=["GET"])
